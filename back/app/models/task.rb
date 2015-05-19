be9404325552e4223a896a25324fca19cc052616
Jonathan Martinez <jamm@jmartinezm.org>
2015-05-19 15:13:11 -0430

    Fix de la validacion de status en el modelo Task

--- back/app/models/task.rb
+++ back/app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ActiveRecord::Base
   enum status: {"pendiente" => 0, "realizado" => 1}
   validates :title, :date, presence: true
   validates :title, length: {minimum: 5, maximum: 50}
-  validates :status, inclusion: {in: %w(pendiente realizado 0 1), message: "valor no admitido"}
+  validates :status, inclusion: {in: :status}
   belongs_to :category
 end
