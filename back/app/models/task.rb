cc23d6c9f2c01fc6c3f206db1e4dbd8bbf40d59f
Jonathan Martinez <jamm@jmartinezm.org>
2015-05-19 11:54:42 -0430

    Fix una coma en el modelo de task

--- back/app/models/task.rb
+++ back/app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ActiveRecord::Base
   enum status: {"pendiente" => 0, "realizado" => 1}
   validates :title, :date, presence: true
   validates :title, length: {minimum: 5, maximum: 50}
-  validates :status inclusion: {in: %w(pendiente realizado 0 1), message: "#{value} valor no admitido"}
+  validates :status, inclusion: {in: %w(pendiente realizado 0 1), message: "#{value} valor no admitido"}
   belongs_to :category
 end
