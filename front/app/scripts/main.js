005f283c1eaf19183695aa3f6d54f28256b0cfd1
Jonathan Martinez <jamm@jmartinezm.org>
2015-05-23 17:57:52 -0430

    Diagramado home

--- front/app/scripts/main.js
+++ front/app/scripts/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	$('select').material_select();
 
+  $('.slider').slider({full_width: true});
+
 	$(function(){
 		var task = $('.tasks');
 		var tasks = new Tasks(task);
