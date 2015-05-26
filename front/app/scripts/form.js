cff45bbd9d45f12da6e459983bcd5bb2a57cdd2e
samuel melgarejo <samuel_021093@hotmail.com>
2015-05-26 13:37:14 -0430

    post de categorias listo

--- front/app/scripts/form.js
+++ front/app/scripts/form.js
@@ -1,19 +1,19 @@
 var postData = (function(){
 
-(function () {
+  (function () {
     $.ajax({
       type: 'get',
       url: 'http://localhost:3000/categories',
       success: function (data) {
         for (var i = 0; i < data.length; i++ ) {
           $('#categories').append(
-                '<option value="' + data[i].id + '">'
-                + data[i].category + 
-                '</option>');
+            '<option value="' + data[i].id + '">'
+            + data[i].category + 
+            '</option>');
         } 
       }, 
       error: function(data) {
-          console.log(data);
+        console.log(data);
       }
     }); 
   }()); 
@@ -26,20 +26,53 @@ var postData = (function(){
     };
 
     $.ajax({
-        type: 'post',
-        url: 'http://localhost:3000/tasks',
-        data: dataTask,
-        success: function(data) {
+      type: 'post',
+      url: 'http://localhost:3000/tasks',
+      data: dataTask,
+      success: function(data) {
         console.log(data);
         console.log("Tarea guardada exitosamente, falta arreglar la maqueta")
       }, 
       error: function(data) {
         console.log(data);
       }
-     });
+    });
   })
 
 });
 
+var postCategory = (function(){
+
+  $('#postEnviar').click(function(){
+    var dataCategory = {
+      "title": $('#category_name').val(),
+    };
+
+    $.ajax({
+      type: 'post',
+      url: 'http://localhost:3000/categories',
+      data: dataCategory,
+      success: function(data) {
+        console.log(data)
+        var header = $('#header');
+        var container = $('#cat-cont');
+        categories = new Categories(container,header);
+        if (data.title.length <= 1 ){
+          Materialize.toast('Categoria No Creada', 3000)
+        }
+        else{
+          Materialize.toast('Categoria Creada', 3000)
+        }
+        $('#category_name').val(' '),
+        postCategory();
+      }, 
+      error: function(data) {
+        console.log(data);
+      }
+    });
+  })
+})
+
 postData();
+postCategory();
 
